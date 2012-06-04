ace134e162be498986f31a4ec6c029149a3810d7
brndmg <brndmg33@gmail.com>
2012-06-04 03:18:28 -0700

    forgot to add the new compilation classes

---
+++ src/util/propnet/architecture/propClassLoader.java
@@ -0,0 +1,49 @@
+package util.propnet.architecture;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class propClassLoader extends ClassLoader{
+
+    public propClassLoader(ClassLoader parent) {
+        super(parent);
+    }
+
+    public Class loadClass(String name) throws ClassNotFoundException {
+        if(!"boolPropNet".equals(name))
+                return super.loadClass(name);
+
+        try {
+            String url = "file:src/boolPropNet.class";
+            URL myUrl = new URL(url);
+            URLConnection connection = myUrl.openConnection();
+            InputStream input = connection.getInputStream();
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            int data = input.read();
+
+            while(data != -1){
+                buffer.write(data);
+                data = input.read();
+            }
+
+            input.close();
+
+            byte[] classData = buffer.toByteArray();
+
+            return defineClass("boolPropNet",
+                    classData, 0, classData.length);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace(); 
+        }
+
+        return null;
+    }
+
+}
